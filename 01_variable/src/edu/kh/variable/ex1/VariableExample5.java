b35bae0725f547be14e6277bc3a375756157d75c
신아민 <qnrlgkals@naver.com>
2022-08-05 16:29:04 +0900

    20220805 수업내용

--- 01_variable/src/edu/kh/variable/ex1/VariableExample5.java
+++ 01_variable/src/edu/kh/variable/ex1/VariableExample5.java
@@ -50,9 +50,13 @@ public class VariableExample5 {
 		// \n 줄바꿈(개행)을 나타내는 탈출(escape) 문자
 		
 		
-		System.out.printf("줄 바꼇나요??");
 		
 		System.out.println("내가 추가한 내용");
+		
+		
+		//주석 
+		System.out.println("새로운 코드");
+		int aaa = 10;
 		
 		
 	}
