8ca2aa6aa630af760853675b7612bab4c8ef929c
Shinamin_97 <qnrlgkals@naver.com>
2022-08-24 17:23:28 +0900

    Exception
    각 예외사항

---
+++ 09_exception/src/edu/kh/exception/model/vo/Parent.java
@@ -0,0 +1,11 @@
+package edu.kh.exception.model.vo;
+
+import java.io.IOException;
+
+public abstract class Parent {
+
+	public abstract void method() throws IOException;
+	
+	
+
+}
---
+++ 09_exception/src/edu/kh/exception/run/Run.java
@@ -0,0 +1,21 @@
+package edu.kh.exception.run;
+
+import edu.kh.exception.model.service.Example;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		Example exam = new Example();
+		
+//		exam.ex1();
+//		exam.ex2();
+//		exam.ex3();
+		exam.ex4();
+		
+		
+		
+	}
+	
+
+}
