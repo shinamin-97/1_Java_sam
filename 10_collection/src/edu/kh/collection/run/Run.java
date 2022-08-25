eba59fded104051d8a3e700a54adfd82bae29994
Shinamin_97 <qnrlgkals@naver.com>
2022-08-25 14:25:15 +0900

    collection
    List

---
+++ 10_collection/src/edu/kh/collection/run/Run.java
@@ -0,0 +1,19 @@
+package edu.kh.collection.run;
+
+import edu.kh.collection.model.service.ListService;
+import edu.kh.collection.view.StudentView;
+
+public class Run {
+	public static void main(String[] args) {
+		
+		ListService service1 = new ListService();
+		
+//		service1.ex1();
+		
+		new StudentView().displayMenu(); // 객체의 1회성 사용
+		
+		
+		
+	}
+
+}
