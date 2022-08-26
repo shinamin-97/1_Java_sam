fba421ac0699de33195ba1ca3418350e15dcadc2
Shinamin_97 <qnrlgkals@naver.com>
2022-08-26 12:50:33 +0900

    io
    Stream

---
+++ 11_io/src/edu/kh/io/run/Run.java
@@ -0,0 +1,14 @@
+package edu.kh.io.run;
+
+import edu.kh.io.service.IOService;
+
+public class Run {
+	public static void main(String[] args) {
+	
+		IOService service = new IOService();
+//		service.output();
+//		service.input();
+		service.fileCopy();
+}
+
+}
